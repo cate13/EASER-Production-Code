7d0ced98354f82ac2838c0628bb2e4b3a4d7a075
BuildTools <claire.cate@yahoo.com>
2026-06-03 16:54:41 -0600

    working kinda

--- database_class.py
+++ database_class.py
@@ -9,7 +9,7 @@ from vectorizers.Empath4D import Empath4DVectorMaker
 from vectorizers.NRC_EIL import NRC_EIL_VectorMaker
 from vectorizers.SentanceBERT import SBERTVectorMaker
 from STEM_classification.cosine_classifier import CosineClassifier
-from helper_functions import combine_using_bilinear_pool
+from helper_functions import combine_using_bilinear_pool, average_vectors
 
 def get_stem_topic_set():
     with open("STEM_classification/STEM_topics.txt", "r", encoding="utf-8") as file:
@@ -19,47 +19,42 @@ def get_stem_topic_set():
 
     return stem_set
 
-class Database():
-    def __init__(self, data_file = "test_files/sampled_output_3_4.jsonl"):
-        self.TOPIC_VECTOR_MAKER = Empath4DVectorMaker()
-        self.EMOTION_VECTOR_MAKER = NRC_EIL_VectorMaker()
-        self.CLASSIFIER_VECTOR_MAKER = SBERTVectorMaker()
-        self.CLASSIFIER_MODEL = joblib.load("STEM_classification/best_model_GaussianNB.joblib")
-        self.COSINE_CLASSIFIER = CosineClassifier()
-        self.STEM_TOPIC_SET = get_stem_topic_set()
-        json_lines = self.read_in_jsonl_to_list(data_file)
-        self.database = self.create_dataset(json_lines)
-        self.create_look_up()
+TOPIC_VECTOR_MAKER = Empath4DVectorMaker()
+EMOTION_VECTOR_MAKER = NRC_EIL_VectorMaker()
+CLASSIFIER_VECTOR_MAKER = SBERTVectorMaker()
+CLASSIFIER_MODEL = joblib.load("STEM_classification/best_model_GaussianNB.joblib")
+COSINE_CLASSIFIER = CosineClassifier()
+STEM_TOPIC_SET = get_stem_topic_set()
 
-    def process_book(self, book_json_object):
+def process_book(book_json_object):
         isbn = book_json_object.get("ISBN")
         description = book_json_object.get("description")
 
-        topic_vec = self.TOPIC_VECTOR_MAKER.getEmapthVector(description)
-        emotion_vec = self.EMOTION_VECTOR_MAKER.getEmotionVector(description)
+        topic_vec = TOPIC_VECTOR_MAKER.getEmapthVector(description)
+        emotion_vec = EMOTION_VECTOR_MAKER.getEmotionVector(description)
 
         # STEM CLASSIFICATION 
         is_STEM = False
         loc_subjects = book_json_object.get("LoC_subjects", [])
         google_categories = book_json_object.get("Google_categories", [])
 
-        has_loc_match = any(subject in self.STEM_TOPIC_SET for subject in loc_subjects)
+        has_loc_match = any(subject in STEM_TOPIC_SET for subject in loc_subjects)
         has_google_match = any(
-            category in self.STEM_TOPIC_SET for category in google_categories
+            category in STEM_TOPIC_SET for category in google_categories
         )
 
         if has_loc_match or has_google_match:
             is_STEM = True
         else:
-            vector_for_classification = self.CLASSIFIER_VECTOR_MAKER.get_vector(description)
+            vector_for_classification = CLASSIFIER_VECTOR_MAKER.get_vector(description)
             formatted_vector_for_classification = np.array(vector_for_classification).reshape(1, -1)
 
-            prediction = self.CLASSIFIER_MODEL.predict(formatted_vector_for_classification)
+            prediction = CLASSIFIER_MODEL.predict(formatted_vector_for_classification)
             predicted_label = prediction[0]
             if predicted_label == 1:
                 is_STEM = True
             else:
-                if self.COSINE_CLASSIFIER.is_stem(topic_vec):
+                if COSINE_CLASSIFIER.is_stem(topic_vec):
                     is_STEM = True
 
         book = {
@@ -70,10 +65,23 @@ class Database():
         }
         return book
 
+def read_in_jsonl_to_list(path):
+    results = []
+    with open(path, "r", encoding="utf-8") as f:
+        for line in f:
+            results.append(json.loads(line))
+    return results
+
+class Database():
+    def __init__(self, data_file = "test_files/sampled_output_3_4.jsonl"):
+        json_lines = read_in_jsonl_to_list(data_file)
+        self.database = self.create_dataset(json_lines)
+        self.create_look_up()
+
     def create_dataset(self, jsonl_list):
         database = []
         for book in jsonl_list:
-            database.append(self.process_book(book))
+            database.append(process_book(book))
         
         database_map = {
             item['isbn']: {
@@ -84,13 +92,6 @@ class Database():
             for item in database
         }
         return database_map
-
-    def read_in_jsonl_to_list(self, path):
-        results = []
-        with open(path, "r", encoding="utf-8") as f:
-            for line in f:
-                results.append(json.loads(line))
-        return results
 
     def create_look_up(self):
         self.isbn_registry = []
@@ -106,8 +107,8 @@ class Database():
         self.nn_model = NearestNeighbors(n_neighbors=len(self.isbn_registry), metric='cosine')
         self.nn_model.fit(X)
 
-    def get_recommendation(self, seed_vec):
-        query_vector = seed_vec.reshape(1, -1)
+    def get_recommendation(self, user):
+        query_vector = user.user_profile_vec.reshape(1, -1)
         distances, indices = self.nn_model.kneighbors(query_vector)
 
         for rank, (idx, dist) in enumerate(zip(indices[0][1:], distances[0][1:]), start=1):
@@ -141,5 +142,22 @@ class Database():
         # this currently doesn't add anything to long term storage
         # so only there while program runs 
 
+
+
+
+class User():
+    def __init__(self, data_file = "test_files/sampled_output_2_4.jsonl"):
+        json_lines = read_in_jsonl_to_list(data_file)
+        self.isbn_list = []
+        book_vecs = []
+        for line in json_lines:
+            book = process_book(line)
+            self.isbn_list.append(book['isbn'])
+            combined_book_vec = combine_using_bilinear_pool(book['emotion_vec'], book['topic_vec'])
+            book_vecs.append(combined_book_vec)
+        self.user_profile_vec = average_vectors(book_vecs)
+            
+    
 database = Database()
-print(type(database.is_book_STEM('0970331010')))
+user = User()
+database.get_recommendation(user)
